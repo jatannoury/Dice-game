acf15675046de000475bf1cb2d3622fbda1bb6cf
josephtannoury <josephtannoury@outlook.com>
2022-05-23 11:56:35 +0300

    Start of the assignment

---
+++ JS/dice.js
@@ -0,0 +1,9 @@
+//storing the paths of images into variables
+const one="images/dice1.png"
+const two="images/dice2.png"
+const three="images/dice3.png"
+const four="images/dice4.png"
+const five="images/dice5.png"
+const six="images/dice6.png"
+ 
+
